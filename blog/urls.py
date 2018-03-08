5eebb5050c4de797e710f11c08f80e9349a96ed0
Satyam-agarwal <agar1satyam.com>
2018-03-08 21:42:41 +0530

    models created and max field is very important which we used in title

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url,include
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+
+urlpatterns =[
+url(r'^$',ListView.as_view(queryset=Post.objects.all(),template_name="blog/blog.html"))]  #vi
