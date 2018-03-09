63f172bcd34919fbdcbaa89ad4d028cc60fddbcd
Satyam-agarwal <agar1satyam.com>
2018-03-09 12:59:16 +0530

    now ppersonl and contact are linked properly by checking out a global urls and personals urls

--- mysite2/urls.py
+++ mysite2/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webapp/', include('webapp.urls')),
     path('personal/', include('personal.urls')),
     path('blog/', include('blog.urls')),
+    url(r'^',include('personal.urls'),)
 ]
